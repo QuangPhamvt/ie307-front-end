182bebe4daab4df4bac672fddb29dbc41182565a
customafk <quangpm220503vt@gmail.com>
2023-11-04 22:09:46 +0700

    feat: add auth api

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React from "react"
 import { RecoilRoot } from "recoil"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import Screen, { DashboardScreen, LogInScreen, RegisterScreen, HomeScreen } from "./src/screen"
+import Screen from "./src/screen"
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RecoilRoot>
+    <RecoilRoot>
+      <NavigationContainer>
         <Screen />
-      </RecoilRoot>
-    </NavigationContainer>
+      </NavigationContainer>
+    </RecoilRoot>
   )
 }
